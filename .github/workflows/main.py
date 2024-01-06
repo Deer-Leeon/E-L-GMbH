8489f18ad1e9515e09072a2b9e2b557d7ca2e17b
Shiny2332 <73343688+Shiny2332@users.noreply.github.com>
2024-01-06 15:39:47 +0100

    Update main.py
    Charakter

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -2,29 +2,38 @@ import pygame
 
 pygame.init()
 
-
-Breite = 442
-Höhe = 50
-
+Bar_Breite = 442
+Bar_Höhe = 50
+SpielerX = 40
+SpielerY = 75
 screen = pygame.display.set_mode((1920, 1080))
+Speed = 5
+positionx = 920
+positiony = 629
 
-
-# Bild laden und auf Fenstergröße skalieren
-hintergrund_bild2 = pygame.image.load('Hintergrund.gif')
-hintergrund_bild = pygame.image.load('Tab.png')  # Passe 'hintergrundbild.jpg' mit dem Namen deines Bildes an
-hintergrund_bild = pygame.transform.scale(hintergrund_bild, (Breite, Höhe))
-hintergrund_bild3 = pygame.image.load('Boden.png')
+hintergrund_bild1 = pygame.image.load('Hintergrund.gif')
+hintergrund_bild3 = pygame.image.load('Tab.png')
+hintergrund_bild2 = pygame.image.load('Boden.png')
 
 running = True
 while running:
+    pygame.time.delay(0)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Hintergrundbild zeichnen
+    
+    laufen = pygame.key.get_pressed()
+    if laufen[pygame.K_LEFT]:
+        positionx = positionx - Speed
+    if laufen[pygame.K_RIGHT]:
+        positionx = positionx + Speed
+
+    
+    screen.blit(hintergrund_bild1, (0, 0))
     screen.blit(hintergrund_bild2, (0, 0))
-    screen.blit(hintergrund_bild3, (0, 0))
-    screen.blit(hintergrund_bild, (737, 1000))
+    screen.blit(hintergrund_bild3, (737, 1000))
+    pygame.draw.rect(screen,(255,0,0), (positionx,positiony,SpielerX,SpielerY))
 
     pygame.display.update()
 

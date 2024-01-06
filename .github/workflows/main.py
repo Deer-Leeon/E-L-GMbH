32eb19ede7d7e27505b3307c048d59cc33b76c4f
Shiny2332 <73343688+Shiny2332@users.noreply.github.com>
2024-01-06 14:21:02 +0100

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,36 +1,31 @@
-# This is a basic workflow to help you get started with Actions
+import pygame
 
-name: CI
+pygame.init()
 
-# Controls when the workflow will run
-on:
-  # Triggers the workflow on push or pull request events but only for the "main" branch
-  push:
-    branches: [ "main" ]
-  pull_request:
-    branches: [ "main" ]
 
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
+Breite = 442
+Höhe = 50
 
-# A workflow run is made up of one or more jobs that can run sequentially or in parallel
-jobs:
-  # This workflow contains a single job called "build"
-  build:
-    # The type of runner that the job will run on
-    runs-on: ubuntu-latest
+screen = pygame.display.set_mode((1920, 1080))
 
-    # Steps represent a sequence of tasks that will be executed as part of the job
-    steps:
-      # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
-      - uses: actions/checkout@v3
 
-      # Runs a single command using the runners shell
-      - name: Run a one-line script
-        run: echo Hello, world!
+# Bild laden und auf Fenstergröße skalieren
+hintergrund_bild2 = pygame.image.load('Hintergrund.gif')
+hintergrund_bild = pygame.image.load('Tab.png')  # Passe 'hintergrundbild.jpg' mit dem Namen deines Bildes an
+hintergrund_bild = pygame.transform.scale(hintergrund_bild, (Breite, Höhe))
+hintergrund_bild3 = pygame.image.load('Boden.png')
 
-      # Runs a set of commands using the runners shell
-      - name: Run a multi-line script
-        run: |
-          echo Add other actions to build,
-          echo test, and deploy your project.
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Hintergrundbild zeichnen
+    screen.blit(hintergrund_bild2, (0, 0))
+    screen.blit(hintergrund_bild3, (0, 0))
+    screen.blit(hintergrund_bild, (737, 1000))
+
+    pygame.display.update()
+
+pygame.quit()
